c222dd05b806dc9280b8ebe93a8171d5f243e575
luanhroliveira <luanhigor98@gmail.com>
2021-07-18 22:07:53 -0300

    Zuul authorization

--- hr-payroll/src/main/java/com/luanhroliveira/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/luanhroliveira/hrpayroll/services/PaymentService.java
@@ -4,14 +4,7 @@ import com.luanhroliveira.hrpayroll.entities.Payment;
 import com.luanhroliveira.hrpayroll.entities.Worker;
 import com.luanhroliveira.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
 
 @Service
 public class PaymentService {
@@ -20,7 +13,6 @@ public class PaymentService {
     private WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(long workerId, int days) {
-
         Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
